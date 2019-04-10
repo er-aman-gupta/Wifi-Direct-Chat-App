37718c0bc8d61bb6915951c62528cab302b8d2ed
er-aman-gupta <er.amangupt99@gmail.com>
2019-04-10 20:56:54 +0530

    Minor Changes

--- app/src/main/java/com/example/bluetoothchatapp/MainActivity.java
+++ app/src/main/java/com/example/bluetoothchatapp/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.bluetoothchatapp;
 
+import android.Manifest;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.net.wifi.WifiManager;
 import android.net.wifi.p2p.WifiP2pConfig;
 import android.net.wifi.p2p.WifiP2pDevice;
@@ -13,6 +15,9 @@ import android.net.wifi.p2p.WifiP2pManager;
 import android.os.Handler;
 import android.os.Message;
 import android.os.StrictMode;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -51,6 +56,24 @@ public class MainActivity extends AppCompatActivity {
     static public Server_Side_Thread serverSideThread;
     static public Client_Thread clientThread;
     static public SendReceiveThread sendReceiveThread;
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if(requestCode==1)
+        {
+            if(grantResults.length>0&&grantResults[0]==PackageManager.PERMISSION_GRANTED)
+            {
+                Toast.makeText(MainActivity.this,"Lets GO!!!!",Toast.LENGTH_LONG).show();
+
+            }
+            else{
+                constate.setText("Cant run the app without location permission\nPlease Give The Permission");
+                //Toast.makeText(MainActivity.this,"Cant run the app without location permission\nPlease Give The Permission",Toast.LENGTH_LONG).show();
+
+                ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},1);
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -96,10 +119,16 @@ public class MainActivity extends AppCompatActivity {
     }
     void initValues()
     {
+        constate=findViewById(R.id.connectionStatus);
+        //Check location permission
+        if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},1);
+        }
+
         enWIFIbtn=findViewById(R.id.WIFIButton);
         discoverbtn=findViewById(R.id.discoverbtn);
 
-        constate=findViewById(R.id.connectionStatus);
 
         listView=findViewById(R.id.peerListView);
         wifiManager= (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
